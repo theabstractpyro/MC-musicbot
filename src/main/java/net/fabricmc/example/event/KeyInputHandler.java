55db240e6c6b7dd75dd8c3fadfd748f2d865de30
Brian Capozza <bfc1410@rit.edu>
2023-09-23 18:16:28 -0400

    various testing

--- src/main/java/net/fabricmc/example/event/KeyInputHandler.java
+++ src/main/java/net/fabricmc/example/event/KeyInputHandler.java
@@ -2,8 +2,11 @@ package net.fabricmc.example.event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import org.lwjgl.glfw.GLFW;
 
@@ -16,9 +19,22 @@ public class KeyInputHandler {
     public static void registerKeyInputs() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (startKey.wasPressed()) {
-                client.player.sendChatMessage("Hello");
-                client.player.setHeadYaw(90);
-                System.out.println(client.player.headYaw);
+                //client.player.setHeadYaw(90);
+                System.out.println(client.player.renderYaw);
+                //client.player.setYaw(90);
+                //client.player.setPitch(0);
+                //Hand hand = client.player.getActiveHand();
+                //client.player.swingHand(hand);
+                //client.mouse.unlockCursor();
+                //BlockEntity noteblock =
+                //client.world.addBlockEntity();
+                BlockPos pos = client.player.getBlockPos();
+                System.out.println(pos);
+                //BlockEntity block = client.world.getBlockEntity(pos);
+                //client.player.interact(block, hand);
+                //client.interactionManager.breakBlock(pos);
+                client.interactionManager.clickButton(1, 1);
+                //System.out.println(client.player.getPos());
                 //System.out.println(client.player);
             }
         });
